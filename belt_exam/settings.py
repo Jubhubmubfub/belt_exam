f395948f624bd9442c40ccbefcf76ef130435365
Marcus Sakoda <Jubhub@yahool.com>
2017-01-27 14:22:54 -0800

    i hate this

--- belt_exam/settings.py
+++ belt_exam/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#^!g5&sufo9)y4l*%$-mp!aq_#)jzdmz&z4uip3*zks3+(j1ca'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.153.102.163']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
